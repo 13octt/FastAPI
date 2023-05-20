c148c15ccf3709d85bd2e89a5707e8d3411b961f
13octt <88072761+13octt@users.noreply.github.com>
2023-05-20 20:31:04 +0700

    Post Data & Device Status

--- Fast_API/fastapi/lab04.py
+++ Fast_API/fastapi/lab04.py
@@ -1,26 +1,33 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from datetime import datetime
-import asyncpg
 
 app = FastAPI()
 
-class SensorData(BaseModel):
+class Data(BaseModel):
     temperature: float
     humidity: float
     light: float
 
-class Time(BaseModel):
-    time_connect: datetime.strftime("%Y-%m-%d %H:%M:%S")
-    time_disconnect: datetime.strftime("%Y-%m-%d %H:%M:%S")
-    status: str
+class Device(BaseModel):
+    name: str
+    connected: bool
+    last_connected: datetime = None
+    last_disconnected: datetime = None
+
 class ApiResponse(BaseModel):
     error: bool
     message: str
-    data: SensorData
-    # current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    data: Data
+    devices: Device
 
-@app.post("/data")
-async def save_sensor_data(data: SensorData):
-    response = ApiResponse(error=False, message="This is a message of API", data=data)
+sensor_data = Data(temperature=0.0, humidity=0.0, light=0.0)
+device = Device(name="My Device", connected=False)
+api = ApiResponse(error=False, message="This is a message of API", data=sensor_data, devices=device)
+
+@app.post("/lab04")
+async def API(info:ApiResponse):
+    # response = ApiResponse(error=False, message="This is a message of API", data=info.data, devices=info.devices)
+    global api
+    response = api
     return response
