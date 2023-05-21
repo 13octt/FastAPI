3059b584bc57fd59b235e9edfdd1a2a0b8e2ffbf
13octt <88072761+13octt@users.noreply.github.com>
2023-05-22 00:00:14 +0700

    update JSON

--- Fast_API/fastapi/lab04.py
+++ Fast_API/fastapi/lab04.py
@@ -1,33 +1,65 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from datetime import datetime
 
 app = FastAPI()
 
-class Data(BaseModel):
-    temperature: float
-    humidity: float
-    light: float
+class Dashboard(BaseModel):
+    device_name: str
+    enabled: bool
+    last_connection_time: int
+    last_disconnection_time: int
 
-class Device(BaseModel):
-    name: str
-    connected: bool
-    last_connected: datetime = None
-    last_disconnected: datetime = None
+class SensorData(BaseModel):
+    temperature: int
+    humidity: int
+    light: int
+
+class Log(BaseModel):
+    device_name: str
+    device_ip: str
+    device_id: int
+    value_name: str
+    value: int
+    received_time: int
 
 class ApiResponse(BaseModel):
     error: bool
     message: str
-    data: Data
-    devices: Device
+    dashboard: Dashboard
+    sensor_data: SensorData
+    log: Log
 
-sensor_data = Data(temperature=0.0, humidity=0.0, light=0.0)
-device = Device(name="My Device", connected=False)
-api = ApiResponse(error=False, message="This is a message of API", data=sensor_data, devices=device)
-
-@app.post("/lab04")
-async def API(info:ApiResponse):
-    # response = ApiResponse(error=False, message="This is a message of API", data=info.data, devices=info.devices)
-    global api
-    response = api
+@app.get("/api")
+async def get_api():
+    dashboard = Dashboard(
+        device_name="Device 1",
+        enabled=True,
+        last_connection_time=1621664400,
+        last_disconnection_time=1621664500
+    )
+    sensor_data = SensorData(
+        temperature=25,
+        humidity=50,
+        light=100
+    )
+    log = Log(
+        device_name="Device 1",
+        device_ip="192.168.1.100",
+        device_id=1,
+        value_name="Value 1",
+        value=10,
+        received_time=1621664600
+    )
+    response = ApiResponse(
+        error=True,
+        message="This is a message of API",
+        dashboard=dashboard,
+        sensor_data=sensor_data,
+        log=log
+    )
     return response
+
+@app.post("/api")
+async def post_api(data: ApiResponse):
+    # Do something with the received data
+    return {"message": "Data received successfully"}
